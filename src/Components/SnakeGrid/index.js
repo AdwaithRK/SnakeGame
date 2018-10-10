f1246300f2ee58da19780efcd61eccac503b6e9c
adwaith <adwaith@qburst.com>
2018-10-10 17:21:38 +0530

    initial commit

---
+++ src/Components/SnakeGrid/index.js
@@ -0,0 +1,46 @@
+
+
+import React from 'react'
+
+
+
+
+export default class SnakeGrid extends React.Component{
+
+    componentDidMount() {
+        this.updateCanvas();
+    }
+    updateCanvas() {
+        const ctx = this.refs.canvas.getContext('2d');
+        ctx.fillRect(0,0, 100, 100);
+    }
+
+    render(){
+        return (
+            <canvas ref="canvas" width={300} height={300}/>
+        );
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
